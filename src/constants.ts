f4370ec0f166c09c747d07b4dee813dcbef0f93d
Yoojin Ko <db232100@gmail.com>
2023-09-01 20:43:18 +0900

    feat: add paths

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,26 @@ export const toResponsePath = (requester:string, appName: string, serviceName: s
   return `/apps/${appName}/service/${serviceName}/${requester}/${timestampStr}/response`;
 }
 
+export const Path = {
+  app: (appName: string): any => {
+    return {
+      root: `apps/${appName}`,
+      balance: `${Path.app(appName).root}/balance`,
+      balanceOfUser: (userAddress: string) => `${Path.app(appName).balance}/${userAddress}/balance`,
+      deposit: `${Path.app(appName).root}/deposit`,
+      depositOfUser: (userAddress: string) => `${Path.app(appName).deposit}/${userAddress}`,
+      billingConfig: `${Path.app(appName).root}/billingConfig`,
+      service: (serviceName: string) => `${Path.app(appName).root}/service/${serviceName}`,
+      userOfService: (serviceName: string, userAddress: string) => 
+        `${Path.app(appName).service(serviceName)}/${userAddress}`,
+      request: (serviceName: string, userAddress: string, requestKey: string) =>
+        `${Path.app(appName).userOfService(serviceName, userAddress)}/${requestKey}/request`,
+      response: (serviceName: string, userAddress: string, requestKey: string) =>
+        `${Path.app(appName).userOfService(serviceName, userAddress)}/${requestKey}/response`
+    }
+  }
+}
+
 export const SECOND = 1000;
 export const HANDLER_TIMEOUT = 30 * SECOND;
 export const HANDLER_HEARBEAT_INTERVAL = 15 * SECOND;
