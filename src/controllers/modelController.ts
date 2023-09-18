67f8d17b2bb0e38960f6cc06730648036df77b4b
akaster99 <akaster99@comcom.ai>
2023-09-18 17:08:54 +0900

    feat: modelSkeleton

---
+++ src/controllers/modelController.ts
@@ -0,0 +1,65 @@
+export default class ModelController {
+  instance: ModelController | undefined;
+  constructor() {
+    if(this.instance) return this.instance;
+    this.instance = this;
+  }
+
+  //TODO(woojae): implement this
+  async isRunning(modelName: string) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  async getInformation(modelName: string) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  async calculateCost(modelName: string, requestData: string) {
+    return await 0.3;
+  }
+
+  //TODO(woojae): implement this
+  async chargeCredit(modelName: string, amount: number) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  async withdrawCredit(modelName: string, amount: number) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  async getCreditBalance(modelName: string) {
+    return await 0.3;
+  }
+
+  //TODO(woojae): implement this
+  async getCreditHistory(modelName: string) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  async use(modelName: string, requestData: string) {
+    return await true;
+  }
+
+  //TODO(woojae): implement this
+  //NOTE(woojae): need admin
+  async run(modelName: string) {
+    return await true; 
+  }
+
+  //TODO(woojae): implement this
+  //NOTE:(woojae): need admin
+  async stop(modelName: string) {
+    return await true;
+  }
+
+  //TODO:(woojae): implement this
+  //NOTE:(woojae): need admin
+  async changeModelInfo(modelName: string, config: any) {
+    return await true;
+  }
+}
---
+++ src/model.ts
@@ -0,0 +1,68 @@
+import ModelController from "./controllers/modelController";
+
+export default class Model {
+  modelName: string;
+  modelController: ModelController;
+
+  constructor(modelName: string) {
+    this.modelName = modelName;
+    this.modelController = new ModelController();
+  }
+  //TODO(woojae): login not Required
+  async isRunning() {
+    return await this.modelController.isRunning(this.modelName);
+  }
+
+  //TODO(woojae): login not Required
+  async getInformation() {
+    return await this.modelController.getInformation(this.modelName);
+  }
+
+  //TODO(woojae): login not Required
+  async calculateCost (requestData: string) {
+    return await this.modelController.calculateCost(this.modelName, requestData);
+  }
+
+  async chargeCredit(amount: number) {
+    return await this.modelController.chargeCredit(this.modelName, amount);
+  }
+
+  async withdrawCredit(amount: number) {
+    return await this.modelController.withdrawCredit(this.modelName, amount);
+  }
+
+  async getCreditBalance() {
+    return await this.modelController.getCreditBalance(this.modelName);
+  }
+
+  async getCreditHistory() {
+    return await this.modelController.getCreditHistory(this.modelName);
+  }
+
+  async use(requestData: string) {
+    return await this.modelController.use(this.modelName, requestData);
+  }
+
+  //NOTE(woojae): need admin
+  async run() {
+    await this.isAdmin();
+    return await this.modelController.run(this.modelName);
+  }
+
+  //NOTE(woojae): need admin
+  async stop() {
+    await this.isAdmin();
+    return await this.modelController.stop(this.modelName);
+  }
+
+  //NOTE(woojae): need admin
+  async changeModelInfo(config: any) {
+    await this.isAdmin();
+    return await this.modelController.changeModelInfo(this.modelName, config);
+  }
+
+  //TODO(woojae): implement this
+  private async isAdmin() {
+    return true;
+  }
+}
