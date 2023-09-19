48672b240bede86a358fd9492b392b0cc0e75bbd
akaster99 <akaster99@comcom.ai>
2023-09-19 10:35:52 +0900

    modelController

--- src/controllers/modelController.ts
+++ src/controllers/modelController.ts
@@ -1,8 +1,16 @@
+import { SetOperation } from "@ainblockchain/ain-js/lib/types";
+import AinModule from "../ain";
+import { Path } from "../constants";
+import { getRequestDepositOp, getTransferOp } from "../utils/util";
+import { buildSetOperation, buildTxBody } from "../utils/builder";
+
 export default class ModelController {
   private static instance: ModelController | undefined;
+  private ain = AinModule.getInstance();
   static getInstance() {
     if(!ModelController.instance){
       ModelController.instance = new ModelController();
+
     }
     return ModelController.instance;
   }
@@ -22,10 +30,19 @@ export default class ModelController {
     return await 0.3;
   }
 
-  //TODO(woojae): implement this
   async chargeCredit(modelName: string, amount: number) {
-    return await true;
+    this.isLoggedIn();
+    const transferKey = Date.now();
+    const userAddress =  this.ain.getDefaultAccount()!.address;
+    const depositAddress = await this.getDepositAddress(modelName);
+    const op_list: SetOperation[]  = [
+      getTransferOp(userAddress, depositAddress, transferKey.toString(), amount),
+      getRequestDepositOp(modelName, userAddress, transferKey.toString(), amount)
+    ] 
+    const txBody = buildTxBody(op_list, transferKey);
+    return this.ain.sendTransaction(txBody);
   }
+  
 
   //TODO(woojae): implement this
   async withdrawCredit(modelName: string, amount: number) {
@@ -42,9 +59,16 @@ export default class ModelController {
     return await true;
   }
 
-  //TODO(woojae): implement this
+  //TODO(woojae): connect with handler
   async use(modelName: string, requestData: string) {
-    return await true;
+    this.isLoggedIn();
+    const requestKey = Date.now();
+    const requesterAddress =  this.ain.getDefaultAccount()!.address;
+    const requestPath = Path.app(modelName).request(requesterAddress, requestKey);
+    const requestOp = buildSetOperation("SET_VALUE", requestPath, {prompt: requestData});
+    const txBody = buildTxBody(requestOp);
+    await this.ain.sendTransaction(txBody);
+    return requestKey;
   }
 
   //TODO(woojae): implement this
@@ -64,5 +88,14 @@ export default class ModelController {
   async changeModelInfo(modelName: string, config: any) {
     return await true;
   }
+  
+  private async getDepositAddress(appName: string) {
+    return await this.ain.getValue(Path.app(appName).billingConfig().defaultAddress);
+  }
 
+  private isLoggedIn() {
+    if(!this.ain.getDefaultAccount())
+      throw new Error('Set defaultAccount First.');
+    return true;
+  }
 }
