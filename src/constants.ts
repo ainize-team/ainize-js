d8ec315ece50334ee8f8c2530254bd1a4c5abd36
Yoojin Ko <db232100@gmail.com>
2023-09-20 13:52:42 +0900

    fix: path

--- src/constants.ts
+++ src/constants.ts
@@ -8,13 +8,14 @@ export const Path = {
   app: (appName: string): any => {
     return {
       root: () => `/apps/${appName}`,
+      status: () => `${Path.app(appName).root()}/status`,
       balance: () => `${Path.app(appName).root()}/balance`,
       balanceOfUser: (userAddress: string) => `${Path.app(appName).balance()}/${userAddress}/balance`,
       historyOfUser: (userAddress: string) => `${Path.app(appName).balance()}/${userAddress}/history`,
       deposit: () => `${Path.app(appName).root()}/deposit`,
       depositOfUser: (userAddress: string) => `${Path.app(appName).deposit()}/${userAddress}`,
       billingConfig: () => `${Path.app(appName).root()}/billingConfig`,
-      service: () => `${Path.app(appName).root()}/service/`,
+      service: () => `${Path.app(appName).root()}/service`,
       userOfService: (userAddress: string) => 
         `${Path.app(appName).service()}/${userAddress}`,
       request: (userAddress: string, requestKey: string) => 
