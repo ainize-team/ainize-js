1383ce3720dc924cff32d171ce6316a4461aafea
akaster99 <akaster99@comcom.ai>
2024-01-04 15:36:14 +0900

    fix: add await

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -48,7 +48,7 @@ export default class ServiceController {
   }
 
   async chargeCredit(serviceName: string, amount: number): Promise<string> {
-    this.checkRunning(serviceName);
+    await this.checkRunning(serviceName);
     const transferKey = Date.now();
     const userAddress = this.ain.getAddress(); 
     const depositAddress = await this.getDepositAddress(serviceName);
@@ -78,7 +78,7 @@ export default class ServiceController {
   }
 
   async request(serviceName: string, requestData: any) : Promise<any> {
-    this.checkRunning(serviceName);
+    await this.checkRunning(serviceName);
     const result = await new Promise(async (resolve, reject) => {
       const requestKey = Date.now();
       const requesterAddress = this.ain.getAddress();
