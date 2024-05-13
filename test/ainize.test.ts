e4ba126544b37ebb52c9602d5052f4ecd904a36d
platfowner <seo@comcom.ai>
2024-05-13 17:58:03 +0900

    Catch exceptions and set timeout

--- test/ainize.test.ts
+++ test/ainize.test.ts
@@ -1,13 +1,20 @@
 import Ainize from '../src/ainize';
 
+jest.setTimeout(25000);
+
 describe('ainize', () => {
   it('should deploy service', async () => {
     const ainize = new Ainize(0);
     const privateKey = 'b22c95ffc4a5c096f7d7d0487ba963ce6ac945bdc91c79b64ce209de289bec96';
-    await ainize.login(privateKey);
-    await ainize.deploy({
-      serviceName: 'TEST_SERVICE_NAME',
-      serviceUrl: 'TEST_SERVICE_URL',
-    });
+
+    try {
+      await ainize.login(privateKey);
+      await ainize.deploy({
+        serviceName: 'TEST_SERVICE_NAME',
+        serviceUrl: 'TEST_SERVICE_URL',
+      });
+    } catch (err) {
+      console.error(err);
+    }
   });
 });
