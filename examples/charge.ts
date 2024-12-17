94dcfaeabb82f9d1ef0846a721b364328169bf36
akaster99 <idavid29@naver.com>
2024-12-17 16:39:43 +0900

    refactor: readme

--- examples/charge.ts
+++ examples/charge.ts
@@ -2,7 +2,7 @@ import { Ainize } from '@ainize-team/ainize-js';
 const ainPrivateKey = ''; // Insert your private key here
 const main = async () => {
   try {
-    const ainize = new Ainize(0);  // 0 for testnet, 1 for mainnet. You can earn testnet AIN at https://faucet.ainetwork.ai/.
+    const ainize = new Ainize(1);  // 0 for testnet, 1 for mainnet. You can earn testnet AIN at https://faucet.ainetwork.ai/.
     await ainize.login(ainPrivateKey);
     console.log('balance: ',await ainize.getAinBalance());
     const model = await ainize.getModel('ainize_free_inference');
